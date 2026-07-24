7adff962177d0096790d91b0575875cc60ded7cf
Seyra Dev <dev@seyra.app>
2026-07-24 03:54:29 +0000

    Add back empty checkAndUpdateSplashImage actual declaration

--- app/src/androidMain/kotlin/com/kyant/backdrop/catalog/PlatformShare.android.kt
+++ app/src/androidMain/kotlin/com/kyant/backdrop/catalog/PlatformShare.android.kt
@@ -383,6 +383,9 @@ actual fun SeyraSplashImage(modifier: Modifier) {
     Box(modifier)
 }
 
+actual fun checkAndUpdateSplashImage(context: Any?) {
+    // No splash image update needed
+}
 
 @Composable
 actual fun LaunchSplashImageUpdater() {
